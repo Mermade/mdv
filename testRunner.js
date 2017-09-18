5206197d3e50ed791404628efd6c2e4b971a2c93
Mike Ralphson <mike.ralphson@gmail.com>
2017-09-18 23:37:36 +0100

    Fix testRunner --version

--- testRunner.js
+++ testRunner.js
@@ -8,33 +8,31 @@ var readfiles = require('node-readfiles');
 var mdv = require('./index.js');
 
 var argv = require('yargs')
-	.usage('testRunner [options] [{path-to-specs}...]')
-	.boolean('debug')
-	.alias('d','debug')
-	.describe('debug','pass debug flag to mdv')
-	.string('encoding')
-	.alias('e','encoding')
-	.default('encoding','utf8')
-	.describe('encoding','encoding for input/output files')
-	.string('fail')
-	.describe('fail','path to specs expected to fail')
-	.alias('f','fail')
-	.boolean('quiet')
-	.alias('q','quiet')
-	.describe('quiet','do not show test passes on console, for CI')
-	.boolean('stop')
-	.alias('s','stop')
-	.describe('stop','stop on first error')
-	.count('verbose')
-	.alias('v','verbose')
-	.describe('verbose','increase verbosity')
-	.help('h')
+    .usage('testRunner [options] [{path-to-specs}...]')
+    .boolean('debug')
+    .alias('d','debug')
+    .describe('debug','pass debug flag to mdv')
+    .string('encoding')
+    .alias('e','encoding')
+    .default('encoding','utf8')
+    .describe('encoding','encoding for input/output files')
+    .string('fail')
+    .describe('fail','path to specs expected to fail')
+    .alias('f','fail')
+    .boolean('quiet')
+    .alias('q','quiet')
+    .describe('quiet','do not show test passes on console, for CI')
+    .boolean('stop')
+    .alias('s','stop')
+    .describe('stop','stop on first error')
+    .count('verbose')
+    .alias('v','verbose')
+    .describe('verbose','increase verbosity')
+    .help('h')
     .alias('h', 'help')
-	.strict()
-	.version(function() {
-		return require('../package.json').version;
-	})
-	.argv;
+    .strict()
+    .version()
+    .argv;
 
 var red = process.env.NODE_DISABLE_COLORS ? '' : '\x1b[31m';
 var green = process.env.NODE_DISABLE_COLORS ? '' : '\x1b[32m';
@@ -49,117 +47,117 @@ var warnings = [];
 var options = argv;
 
 function checkOutput(output) {
-	var result = true;
-	for (var k in output) {
-		if (typeof output[k] === 'number') {
-			if (output[k] > 0) result = false;
-		}
-		if (Array.isArray(output[k])) {
-			if (output[k].length > 0) result = false;
-		}
-	}
-	return result;
+    var result = true;
+    for (var k in output) {
+        if (typeof output[k] === 'number') {
+            if (output[k] > 0) result = false;
+        }
+        if (Array.isArray(output[k])) {
+            if (output[k].length > 0) result = false;
+        }
+    }
+    return result;
 }
 
 function check(file,force,expectFailure) {
-	var result = false;
-	options.expectFailure = expectFailure;
-	options.source = file;
+    var result = false;
+    options.expectFailure = expectFailure;
+    options.source = file;
 
-	var components = file.split(path.sep);
-	var name = components[components.length-1];
+    var components = file.split(path.sep);
+    var name = components[components.length-1];
 
-	if ((name.endsWith('.md')) || force) {
+    if ((name.endsWith('.md')) || force) {
 
-		var srcStr = fs.readFileSync(path.resolve(file),options.encoding);
-		var output = {failed:true};
-		var result = false;
-		try {
-			output = mdv.validate(srcStr,options);
-			result = checkOutput(output);
-		}
-		catch (ex) {
-			console.log(red+'mdv threw an error: '+ex.message);
-			warnings.push('mdv threw '+options.source);
-			result = false;
-		}
+        var srcStr = fs.readFileSync(path.resolve(file),options.encoding);
+        var output = {failed:true};
+        var result = false;
+        try {
+            output = mdv.validate(srcStr,options);
+            result = checkOutput(output);
+        }
+        catch (ex) {
+            console.log(red+'mdv threw an error: '+ex.message);
+            warnings.push('mdv threw '+options.source);
+            result = false;
+        }
 
-		console.log((result ? green : red)+file);
-		if (!result) {
-			console.log(util.inspect(output));
-		}
-		if (expectFailure) result = !result;
-		if (result) {
-			pass++;
-		}
-		else {
-			fail++;
-		}
+        console.log((result ? green : red)+file);
+        if (!result) {
+            console.log(util.inspect(output));
+        }
+        if (expectFailure) result = !result;
+        if (result) {
+            pass++;
+        }
+        else {
+            fail++;
+        }
 
-	}
-	else {
-		result = true;
-	}
-	return result;
+    }
+    else {
+        result = true;
+    }
+    return result;
 }
 
 function processPathSpec(pathspec,expectFailure) {
-	pathspec = path.resolve(pathspec);
-	var stats = fs.statSync(pathspec);
-	if (stats.isFile()) {
-		check(pathspec,true,expectFailure);
-	}
-	else {
-		readfiles(pathspec, {readContents: false, filenameFormat: readfiles.FULL_PATH}, function (err) {
-			if (err) console.log(util.inspect(err));
-		})
-		.then(files => {
-			files = files.sort(function(a,b){
-				if (a<b) return +1;
-				if (a>b) return -1;
-				return 0;
-			});
-			for (var file of files) {
-				check(file,false,expectFailure);
-			}
-		})
-		.catch(err => {
-			console.log(util.inspect(err));
-		});
-	}
+    pathspec = path.resolve(pathspec);
+    var stats = fs.statSync(pathspec);
+    if (stats.isFile()) {
+        check(pathspec,true,expectFailure);
+    }
+    else {
+        readfiles(pathspec, {readContents: false, filenameFormat: readfiles.FULL_PATH}, function (err) {
+            if (err) console.log(util.inspect(err));
+        })
+        .then(files => {
+            files = files.sort(function(a,b){
+                if (a<b) return +1;
+                if (a>b) return -1;
+                return 0;
+            });
+            for (var file of files) {
+                check(file,false,expectFailure);
+            }
+        })
+        .catch(err => {
+            console.log(util.inspect(err));
+        });
+    }
 }
 
 process.exitCode = 1;
 console.log('Gathering...');
 if ((!argv._.length) && (!argv.fail)) {
-	argv._.push('test/pass/');
+    argv._.push('test/pass/');
 }
 for (let pathspec of argv._) {
-	processPathSpec(pathspec,false);
+    processPathSpec(pathspec,false);
 }
 if (argv.fail) {
-	if (!Array.isArray(argv.fail)) argv.fail = [argv.fail];
-	for (let pathspec of argv.fail) {
-		processPathSpec(pathspec,true);
-	}
+    if (!Array.isArray(argv.fail)) argv.fail = [argv.fail];
+    for (let pathspec of argv.fail) {
+        processPathSpec(pathspec,true);
+    }
 }
 
 process.on('exit', function() {
-	if (warnings.length) {
-		warnings.sort();
-		console.log(normal+'\nWarnings:'+yellow);
-		for (var w in warnings) {
-			console.log(warnings[w]);
-		}
-	}
-	if (failures.length) {
-		failures.sort();
-		console.log(normal+'\nFailures:'+red);
-		for (var f in failures) {
-			console.log(failures[f]);
-		}
-	}
-	console.log(normal);
-	console.log('Tests: %s passing, %s failing, %s warnings', pass, fail, warnings.length);
-	process.exitCode = ((fail === 0) && (pass > 0)) ? 0 : 1;
+    if (warnings.length) {
+        warnings.sort();
+        console.log(normal+'\nWarnings:'+yellow);
+        for (var w in warnings) {
+            console.log(warnings[w]);
+        }
+    }
+    if (failures.length) {
+        failures.sort();
+        console.log(normal+'\nFailures:'+red);
+        for (var f in failures) {
+            console.log(failures[f]);
+        }
+    }
+    console.log(normal);
+    console.log('Tests: %s passing, %s failing, %s warnings', pass, fail, warnings.length);
+    process.exitCode = ((fail === 0) && (pass > 0)) ? 0 : 1;
 });
