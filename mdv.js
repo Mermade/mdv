169f706f7a2a6e04f7a8004f3dbb161cc99e1436
derberg <lpgornicki@gmail.com>
2021-02-03 16:58:53 +0100

    fix: add support for wildcards for windows

--- mdv.js
+++ mdv.js
@@ -6,6 +6,7 @@
 const fs = require('fs');
 const util = require('util');
 const yaml = require('yaml');
+const glob = require("glob");
 
 const validator = require('./index.js');
 
@@ -32,41 +33,47 @@ const argv = require('yargs')
 
 let exitCode = 0;
 const options = argv;
+
+
 for (let a of argv._) {
-    const s = fs.readFileSync(a,'utf8');
-    options.source = a;
-    const result = validator.validate(s,options);
+    glob(a, options, function (er, files) {
+        for (let file of files) {
+            const s = fs.readFileSync(a,'utf8');
+            options.source = file;
+            const result = validator.validate(s,options);
 
-    if (options.save) {
-        fs.writeFileSync(a+'.html',options.html,'utf8');
-        delete options.html;
-    }
+            if (options.save) {
+                fs.writeFileSync(a+'.html',options.html,'utf8');
+                delete options.html;
+            }
 
-    let ok = true;
-    for (let p in result) {
-        if (typeof result[p] == 'number') {
-            if (result[p]) ok = false
-            else delete result[p];
+            let ok = true;
+            for (let p in result) {
+                if (typeof result[p] == 'number') {
+                    if (result[p]) ok = false
+                    else delete result[p];
+                }
+                else if (Array.isArray(result[p])) {
+                    if (result[p].length) ok = false
+                    else delete result[p];
+                }
+                else if (typeof result[p] == 'object') {
+                    if (Object.keys(result[p]).length) ok = false
+                    else delete result[p];
+                }
+            }
+            if (!ok) {
+                if (argv.yaml) {
+                console.log(yaml.stringify(result));
+                }
+                else {
+                console.log(util.inspect(result,{depth:null}));
+                }
+                exitCode = 1;
+            }
         }
-        else if (Array.isArray(result[p])) {
-            if (result[p].length) ok = false
-            else delete result[p];
-        }
-        else if (typeof result[p] == 'object') {
-            if (Object.keys(result[p]).length) ok = false
-            else delete result[p];
-        }
-    }
-    if (!ok) {
-        if (argv.yaml) {
-          console.log(yaml.stringify(result));
-        }
-        else {
-          console.log(util.inspect(result,{depth:null}));
-        }
-        exitCode = 1;
-    }
-
+    })
 }
+
 
 process.exit(exitCode);
