a10dfb52935ab1c1b5853079a39665a6d0285598
derberg <lpgornicki@gmail.com>
2021-02-04 09:56:26 +0100

    trying different rule for linebreak style

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": "./.eslintrc.json",
+    "rules": {
+        // override default options
+       'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix']
+    }
+};
